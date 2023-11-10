10fcf9341e2d7e03244db113f89fb2094a44127a
dongwoo <comich1994@naver.com>
2023-11-10 23:22:03 +0900

    [fea] admissionController 구현

--- kubernetes/controller/dummycron/api/v1/cronjob_webhook.go
+++ kubernetes/controller/dummycron/api/v1/cronjob_webhook.go
@@ -17,7 +17,11 @@ limitations under the License.
 package v1
 
 import (
+	"github.com/robfig/cron"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -25,6 +29,7 @@ import (
 )
 
 // log is for logging in this package.
+// webhook용 로거 설정
 var cronjoblog = logf.Log.WithName("cronjob-resource")
 
 // SetupWebhookWithManager will setup the manager to manage the webhooks
@@ -36,6 +41,7 @@ func (r *CronJob) SetupWebhookWithManager(mgr ctrl.Manager) error {
 
 // TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
+// ref: https://book.kubebuilder.io/reference/markers/webhook
 //+kubebuilder:webhook:path=/mutate-batch-my-kubebuilder-v1-cronjob,mutating=true,failurePolicy=fail,sideEffects=None,groups=batch.my.kubebuilder,resources=cronjobs,verbs=create;update,versions=v1,name=mcronjob.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Defaulter = &CronJob{}
@@ -44,7 +50,20 @@ var _ webhook.Defaulter = &CronJob{}
 func (r *CronJob) Default() {
 	cronjoblog.Info("default", "name", r.Name)
 
-	// TODO(user): fill in your defaulting logic.
+	if r.Spec.ConcurrencyPolicy == "" {
+		r.Spec.ConcurrencyPolicy = AllowConcurrent
+	}
+	if r.Spec.Suspend == nil {
+		r.Spec.Suspend = new(bool)
+	}
+	if r.Spec.SuccessfulJobsHistoryLimit == nil {
+		r.Spec.SuccessfulJobsHistoryLimit = new(int32)
+		*r.Spec.SuccessfulJobsHistoryLimit = 3
+	}
+	if r.Spec.FailedJobsHistoryLimit == nil {
+		r.Spec.FailedJobsHistoryLimit = new(int32)
+		*r.Spec.FailedJobsHistoryLimit = 1
+	}
 }
 
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
@@ -56,8 +75,7 @@ var _ webhook.Validator = &CronJob{}
 func (r *CronJob) ValidateCreate() (admission.Warnings, error) {
 	cronjoblog.Info("validate create", "name", r.Name)
 
-	// TODO(user): fill in your validation logic upon object creation.
-	return nil, nil
+	return nil, r.validateCronJob()
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
@@ -65,7 +83,7 @@ func (r *CronJob) ValidateUpdate(old runtime.Object) (admission.Warnings, error)
 	cronjoblog.Info("validate update", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object update.
-	return nil, nil
+	return nil, r.validateCronJob()
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
@@ -73,5 +91,46 @@ func (r *CronJob) ValidateDelete() (admission.Warnings, error) {
 	cronjoblog.Info("validate delete", "name", r.Name)
 
 	// TODO(user): fill in your validation logic upon object deletion.
-	return nil, nil
+	return nil, r.validateCronJob()
+}
+
+func (r *CronJob) validateCronJob() error {
+	var allErrs field.ErrorList
+	if err := r.validateCronJobName(); err != nil {
+		allErrs = append(allErrs, err)
+	}
+	if err := r.validateCronJobSpec(); err != nil {
+		allErrs = append(allErrs, err)
+	}
+	if len(allErrs) == 0 {
+		return nil
+	}
+	return apierrors.NewInvalid(
+		schema.GroupKind{Group: "batch.my.kubebuilder", Kind: "CronJob"},
+		r.Name, allErrs)
+
+}
+
+func (r *CronJob) validateCronJobName() *field.Error {
+	if len(r.Name) > 5 { // 테스트용으로 5글자 이상을 금지시켜봅시다
+		return field.Invalid(field.NewPath("metadata").Child("name"), r.Name, "name must be no more than 5 characters")
+	}
+	return nil
+}
+
+func (r *CronJob) validateCronJobSpec() *field.Error {
+	// concurrency policy도 금지시켜보자
+	if r.Spec.ConcurrencyPolicy != ForbidConcurrent && r.Spec.ConcurrencyPolicy != AllowConcurrent {
+		return field.Invalid(field.NewPath("spec").Child("concurrencyPolicy"), r.Spec.ConcurrencyPolicy, "concurrencyPolicy must be Forbid or Allow")
+	}
+	return validateScheduleFormat( //cronjob format을 검증하는 함수
+		r.Spec.Schedule,
+		field.NewPath("spec").Child("schedule"))
+}
+
+func validateScheduleFormat(schedule string, fldPath *field.Path) *field.Error {
+	if _, err := cron.ParseStandard(schedule); err != nil {
+		return field.Invalid(fldPath, schedule, err.Error())
+	}
+	return nil
 }
